7cf2ed1f5871f4d51b85c1273817286e187e5891
csm <2260236365@qq.com>
2024-06-02 16:56:35 +0800

    二分查找

--- untitled/src/main/java/test/Test.java
+++ untitled/src/main/java/test/Test.java
@@ -10,6 +10,7 @@ public class Test {
         int[] a ={1,2,3,4,5,6,7,8,9,11,22,33,44,55,56,67,678,990,9088};
         int sq = sq(a, 44);
         System.out.println(sq);
+
     }
     public static int sq(int[] i,int a){
         int min=0;
